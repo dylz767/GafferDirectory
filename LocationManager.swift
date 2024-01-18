4f4b1d421a2eb3ebdca6f4ffa7eda4d7612e1176
dylz767 <d.angol@live.co.uk>
2024-01-18 22:37:28 +0000

    Job posting functionality improved. Opens the recently posted job. Also Users are signed up correctly and location specifically city is stored in database

---
+++ LocationManager.swift
@@ -0,0 +1,18 @@
+//
+//  LocationManager.swift
+//  GafferDirectory
+//
+//  Created by Dylon Angol on 18/01/2024.
+//
+
+import SwiftUI
+
+struct LocationManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationManager()
+}

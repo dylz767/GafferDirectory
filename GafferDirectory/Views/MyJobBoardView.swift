c6667984e2bc23d9c38e7c242bfe267f45c255a2
dylz767 <d.angol@live.co.uk>
2024-01-18 20:28:39 +0000

    Outstanding favourites button functionality. Now I need to be able to add users to a job posting and add a search bar allowing users to search jobs in a chosen city as well as search users who are based in a certain city.

---
+++ GafferDirectory/Views/MyJobBoardView.swift
@@ -0,0 +1,18 @@
+//
+//  MyJobBoardView.swift
+//  GafferDirectory
+//
+//  Created by Dylon Angol on 18/01/2024.
+//
+
+import SwiftUI
+
+struct MyJobBoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyJobBoardView()
+}
